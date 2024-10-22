fd0cf02d9d040745fdcac9f6a6becc9efcf389d2
Kumiho <enotmos@yandex.ru>
2024-10-22 20:15:47 +0300

    added option -f and arguments in bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,12 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('filepath1')
+  .argument('filepath2')
 
 program.parse();
 
 const options = program.opts();
+console.log(options);
 console.log(program.args);
