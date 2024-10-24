92a2631f1920e9fd5250528437f9f577e32af15d
Kumiho <enotmos@yandex.ru>
2024-10-24 12:48:21 +0300

    added action() in bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
-  .argument('filepath1')
-  .argument('filepath2');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
 
