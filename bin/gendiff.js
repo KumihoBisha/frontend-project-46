2069b7c965c286053f0d3f28c514926f815d2896
Kumiho <enotmos@yandex.ru>
2024-10-28 18:47:41 +0300

    fixed bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,14 +6,10 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((file1, file2) => {
+    console.log(genDiff(file1, file2));
   });
 
 program.parse();
-
-const options = program.opts();
-console.log(options);
-console.log(program.args);
