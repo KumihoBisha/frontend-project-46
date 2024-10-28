c4ae65ce4cc8a9c106d6888a0e476f48b9dd2a0c
Kumiho <enotmos@yandex.ru>
2024-10-28 18:46:47 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parse.js';
+import getDifference from './getDifference.js';
 
 const getParsedFile = (filepath) => {
   const format = path.extname(filepath);
-  const data = fs.readFileSync(filepath, 'utf8');
+  const data = fs.readFileSync(filepath);
   return parse(data, format);
 };
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = getParsedFile(filepath1);
   const data2 = getParsedFile(filepath2);
-  console.log(data1);
-  console.log(data2);
+  return getDifference(data1, data2);
 };
 
 export default genDiff;
