46dea37edef0c3f6fa48bb4345cb1bd7cfe8c91e
Kumiho <enotmos@yandex.ru>
2025-03-26 20:37:34 +0300

    update __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,28 @@
-import fs from 'fs';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import path from 'path';
+import path, { dirname } from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-test('compare two flat files', () => {
-  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const excpectedResult = readFile('expected.txt');
-  expect(result).toEqual(excpectedResult);
+const expectedStylish = readFixture('expected-stylish.txt');
+const expectedPlain = readFixture('expected-plain.txt');
+const expectedJson = readFixture('expected-json.txt');
+
+const formats = ['json', 'yml'];
+
+describe('gendiff', () => {
+  test.each(formats)('compare files with each supported format', (format) => {
+    const filePath1 = getFixturePath(`file1.${format}`);
+    const filePath2 = getFixturePath(`file2.${format}`);
+
+    expect(genDiff(filePath1, filePath2)).toEqual(expectedStylish);
+    expect(genDiff(filePath1, filePath2, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(filePath1, filePath2, 'json')).toEqual(expectedJson);
+  });
 });
