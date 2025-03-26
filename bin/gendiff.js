db85bd26181970e56b745350fe7e96ba7ce3df5d
Kumiho <enotmos@yandex.ru>
2025-03-26 20:59:24 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,22 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
 import genDiff from '../src/index.js';
 
+const program = new Command();
+
 program
-  .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference')
+  .version('0.0.1')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
-  .action((file1, file2) => {
-    console.log(genDiff(file1, file2));
-  });
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format (stylish, plain)', 'stylish');
+
+program.action((filepath1, filepath2, options) => {
+  console.log(genDiff(filepath1, filepath2, options.format));
+});
 
 program.parse();
+
+export default program;
